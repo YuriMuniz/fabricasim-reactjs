e9d8c3bace214dc3d9b3ce6b469e13fed484fddd
YMG <y.munizgonzalez@gmail.com>
2020-04-21 16:11:55 -0300

    feat: estilização página de auth e register

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,7 +1,24 @@
 import React from "react";
 
+import { Link } from "react-router-dom";
+
+import logo from "../../assets/logo.png";
+
 // import { Container } from './styles';
 
 export default function SignIn() {
-  return <h1>Login</h1>;
+  return (
+    <>
+      <img src={logo} alt="Fábrica Sim" />
+
+      <form>
+        <input type="email" placeholder="Seu e-mail" />
+        <input type="password" placeholder="Sua senha" />
+
+        <button type="submit">Acessar</button>
+
+        <Link to="/register">Criar conta</Link>
+      </form>
+    </>
+  );
 }
--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,7 +1,29 @@
 import React from "react";
 
+import { Link } from "react-router-dom";
+
+import logo from "../../assets/logo.png";
+
 // import { Container } from './styles';
 
-export default function SignUp() {
-  return <h1>Cadastro</h1>;
+export default function SignIn() {
+  return (
+    <>
+      <img src={logo} alt="Fábrica Sim" />
+
+      <form>
+        <input type="email" placeholder="E-mail" />
+        <input type="text" placeholder="Ocupação" />
+        <input type="text" placeholder="Celular" />
+        <input type="text" placeholder="Páis" />
+        <input type="text" placeholder="Estado" />
+        <input type="password" placeholder="Senha" />
+        <input type="password" placeholder="Confirmar senha" />
+
+        <button type="submit">Criar conta</button>
+
+        <Link to="/">Já tenho login</Link>
+      </form>
+    </>
+  );
 }
