8a5db3587f87db36a1b3781886ee657b6c6f7452
YMG <y.munizgonzalez@gmail.com>
2020-04-21 18:52:02 -0300

    feat: funcionalde de select e input de paises e estaados

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { Link } from "react-router-dom";
 
 import logo from "../../assets/logo-full.png";
 
-import { Images } from "./styles";
+import listHelper from "../../util/listHelper";
+
+import { Images, Select, SelectState, InputState } from "./styles";
 
 export default function SignIn() {
+  // const [country, setCountry] = useState();
+  const [isBr, setIsBr] = useState(false);
+
+  function handleCountryChange(event) {
+    console.log(event.target.value);
+    if (event.target.value === "Brazil") {
+      setIsBr(true);
+    } else {
+      setIsBr(false);
+    }
+  }
+
   return (
     <>
       <Images>
@@ -18,8 +32,26 @@ export default function SignIn() {
         <input type="email" placeholder="E-mail" />
         <input type="text" placeholder="Ocupação" />
         <input type="text" placeholder="Celular" />
-        <input type="text" placeholder="País" />
-        <input type="text" placeholder="Estado" />
+        <Select placeholder="País" size="number" onChange={handleCountryChange}>
+          <option value="1" disabled selected>
+            País
+          </option>
+          {listHelper.Countries.map((c) => (
+            <option key={c.key} value={c.key}>
+              {c.view}
+            </option>
+          ))}
+          ;
+        </Select>
+        <SelectState isBr={isBr} placeholder="País" size="number">
+          {listHelper.States.map((s) => (
+            <option key={s.key} value={s.key}>
+              {s.view}
+            </option>
+          ))}
+          ;
+        </SelectState>
+        <InputState isBr={isBr} type="text" placeholder="Estado" />
         <input type="password" placeholder="Senha" />
         <input type="password" placeholder="Confirmar senha" />
 
--- src/pages/SignUp/styles.js
+++ src/pages/SignUp/styles.js
@@ -8,3 +8,61 @@ export const Images = styled.div`
 
   height: 60px;
 `;
+
+export const Select = styled.select`
+  background: rgba(0, 0, 0, 0.1);
+  border: 0;
+  border-radius: 4px;
+  height: 44px;
+  padding: 0 15px;
+  font-size: 14px;
+  color: rgba(255, 255, 255, 0.7);
+  margin: 0 0 10px;
+
+  option {
+    color: rgba(255, 255, 255, 0.7);
+    background: #243943;
+    display: flex;
+    white-space: pre;
+    min-height: 20px;
+    padding: 0px 2px 1px;
+  }
+`;
+
+export const SelectState = styled.select`
+  background: rgba(0, 0, 0, 0.1);
+  border: 0;
+  border-radius: 4px;
+  height: 44px;
+  padding: 0 15px;
+  font-size: 14px;
+  color: rgba(255, 255, 255, 0.7);
+  margin: 0 0 10px;
+
+  option {
+    color: rgba(255, 255, 255, 0.7);
+    background: #243943;
+    display: flex;
+    white-space: pre;
+    min-height: 20px;
+    padding: 0px 2px 1px;
+  }
+
+  display: ${(props) => (props.isBr ? "block" : "none")};
+`;
+
+export const InputState = styled.input`
+  background: rgba(0, 0, 0, 0.1);
+  border: 0;
+  border-radius: 4px;
+  height: 44px;
+  padding: 0 15px;
+  color: #fff;
+  margin: 0 0 10px;
+
+  &::placeholder {
+    color: rgba(255, 255, 255, 0.7);
+  }
+
+  display: ${(props) => (props.isBr ? "none" : "block")};
+`;
