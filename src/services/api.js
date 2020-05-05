edc47e237e86a0d7061182e4e01e367aee53b2ee
YMG <y.munizgonzalez@gmail.com>
2020-05-05 15:34:55 -0300

    chore: att

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://206.189.236.92:3333/",
+  baseURL: "http://206.189.236.92:3333",
   headers: {
     "Content-Type": "application/json;charset=UTF-8",
   },
