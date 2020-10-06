2d9e556192bd8cf6d382e6be4c64d044dc64d4e6
YMG <y.munizgonzalez@gmail.com>
2020-10-05 22:42:28 -0300

    ajuste

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  //baseURL: "https://api-portal.fabricasim.com",
-  baseURL: "http://localhost:3333",
+  baseURL: "https://api-portal.fabricasim.com",
+  //baseURL: "http://localhost:3333",
   headers: {
     "Content-Type": "application/json;charset=UTF-8",
   },
